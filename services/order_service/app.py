41cf9baeb3f6a6a23fb329ec2aec407c38470564
Mariam Essam <mariam.non30@gmail.com>
2025-12-25 05:34:45 +0200

    Update order service

--- services/order_service/app.py
+++ services/order_service/app.py
@@ -1,31 +1,27 @@
 from flask import Flask, request, jsonify
-import mysql.connector
 from mysql.connector import Error
 import requests  # to call Inventory Service
-
+from db import get_db_connection
 app = Flask(__name__)
 
-# Connect to MySQL
-try:
-    db_conn = mysql.connector.connect(
-        host="localhost",
-        user="ecommerce_user",
-        password="secure_password",
-        database="ecommerce_system"
-    )
-    cursor = db_conn.cursor(dictionary=True)
-    print("Connected to MySQL database")
-except Error as e:
-    print(f"Error connecting to MySQL: {e}")
-    exit(1)
+@app.route('/')
+def home():
+    return "Order service is running!"
 
 @app.route('/api/orders/create', methods=['POST'])
 def create_order():
+    db_conn = get_db_connection()
+    cursor = db_conn.cursor(dictionary=True)
     try:
         data = request.get_json()
+        if not data:
+            return jsonify({"error": "Invalid JSON"}), 400
+        
         customer_id = data['customer_id']
         products = data['products']
         total_amount = data['total_amount']
+        if not customer_id or not products or total_amount is None:
+            return jsonify({"error": "Missing required fields"}), 400
 
         # Insert order
         cursor.execute(
@@ -37,6 +33,8 @@ def create_order():
 
         # Insert products for the order
         for p in products:
+            if p['quantity'] <= 0:
+                return jsonify({"error": "Quantity must be > 0"}), 400
             cursor.execute(
                 "INSERT INTO orders_products (order_id, product_id, quantity) VALUES (%s, %s, %s)",
                 (order_id, p['product_id'], p['quantity'])
@@ -53,10 +51,65 @@ def create_order():
         except Exception as inv_err:
             print(f"Failed to update inventory: {inv_err}")
 
+        #Automatically update customer loyalty points
+        try:
+            requests.put(
+                f"http://localhost:5004/api/customers/{customer_id}/loyalty",
+                json={"points": 10},
+                timeout=3
+            )
+        except Exception as e:
+            print("Loyalty update failed:", e)
+
+        #Automatically send notification
+
+        try:
+            requests.post(
+                "http://localhost:5005/api/notifications/send",
+                json={
+                    "order_id": order_id,
+                    "customer_id": customer_id
+                },
+                timeout=3
+            )
+        except Exception as e:
+            print("Notification failed:", e)
+
+
         return jsonify({"order_id": order_id, "status": "success"}), 201
 
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)}), 500
 
-if __name__ == "__main__":
+@app.route('/api/orders/<int:order_id>', methods=['GET'])
+def get_order(order_id):
+    db_conn = get_db_connection()
+    cursor = db_conn.cursor(dictionary=True)
+    cursor.execute(
+    "SELECT * FROM orders WHERE order_id = %s",
+    (order_id,)
+    )
+    order = cursor.fetchone()
+
+    if not order:
+        cursor.close()
+        db_conn.close()
+        return jsonify({"error": "Order not found"}), 404
+    
+    cursor.execute(
+        "SELECT product_id, quantity FROM orders_products WHERE order_id = %s",
+        (order_id,)
+    )
+    products = cursor.fetchall()
+
+    cursor.close()
+    db_conn.close()
+
+    return jsonify({
+        "order": order,
+        "products": products
+    }), 200
+
+if __name__ == '__main__':
     app.run(port=5001, debug=True)
+                    
